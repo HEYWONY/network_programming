337a4150edc6ff20141398399fbf0f20c3f7ca85
Chawwon <special_1106@naver.com>
2021-04-07 23:16:27 +0900

    hw5

---
+++ HW5_CHAEWON_KIM/server.py
@@ -0,0 +1,35 @@
+from socket import *
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 3333))
+s.listen(5)
+print('waiting...')
+
+while True:
+    client, addr = s.accept()
+    print('connection from ', addr)
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+
+        formula = data.decode()
+        n_list = formula.split(' ')
+
+        try:
+            if n_list[1] == '+':
+                result = int(n_list[0]) + int(n_list[2])
+            elif n_list[1] == '-':
+                result = int(n_list[0]) - int(n_list[2])
+            elif n_list[1] == '*':
+                result = int(n_list[0]) * int(n_list[2])
+            elif n_list[1] == '/':
+                result = round(int(n_list[0]) / int(n_list[2]), 1)
+        except:
+            client.send(b'다시 시도하세요.') 
+        else:           
+            bytes_str_result = str(result).encode()  
+            client.send(bytes_str_result) 
+    
+    client.close()
+s.close()
